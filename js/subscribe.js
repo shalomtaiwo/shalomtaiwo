50c9f37e8905fb1c30f142951c3cf2e7d31350c7
Shalom Taiwo <shalomtaiwo@my.uopeople.edu>
2021-12-31 11:55:38 +0200

    subs

--- js/subscribe.js
+++ js/subscribe.js
@@ -2,6 +2,7 @@ const mailjet = require ('node-mailjet')
 .connect('c491672eff8c735733a70a3eee7763fd', '2e9b84cc8a6a039cca5ba09c94d9da02')
 const request = mailjet
 .post("send", {'version': 'v3.1'})
+.id(subscribe)
 .request({
   "Messages":[
     {
